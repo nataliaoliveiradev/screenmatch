a0176d54d565e45f76c0956fd0976a193069d0f3
nataliaoliveiradev <profissionalnataliaoliveira@gmail.com>
2025-01-19 19:21:25 -0300

    Cria a classe Principal e a classe Filme

---
+++ src/Principal.java
@@ -0,0 +1,18 @@
+public class Principal {
+    public static void main(String[] args)
+    {
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O Poderoso Chefão";
+        meuFilme.anoDeLancamento = 1972;
+        meuFilme.duracaoEmMinutos = 175;
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(9);
+        meuFilme.avalia(10);
+        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println(meuFilme.totalDeAvaliacoes);
+
+
+    }
+}
