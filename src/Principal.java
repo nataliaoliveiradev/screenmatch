2d0807400b0f3a7a0a71a75a148b6839dc973e0b
nataliaoliveiradev <profissionalnataliaoliveira@gmail.com>
2025-01-19 21:59:38 -0300

    Adiciona a classe Serie.java e Titulo.java, bem como define herança da classe Titulo.java para Serie.java e Filme.java

--- src/Principal.java
+++ src/Principal.java
@@ -1,13 +1,15 @@
 import br.com.alulra.screenmatch.modelos.Filme;
+import br.com.alulra.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args)
     {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O Poderoso Chefão";
-        meuFilme.anoDeLancamento = 1972;
-        meuFilme.duracaoEmMinutos = 175;
-        meuFilme.nome.
+        meuFilme.setNome("O Poderoso Chefão");
+        meuFilme.setAnoDeLancamento(1972);
+        meuFilme.setDuracaoEmMinutos(175);
+        System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
+
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
@@ -15,6 +17,16 @@ public class Principal {
         meuFilme.avalia(10);
         System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println("Média de avaliações: " + meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2004);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(6);
+        lost.setEpisodiosPorTemporada(21);
+        lost.setAtiva(false);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Quanto tempo para maratonar " + lost.getNome() + ": " + lost.getDuracaoEmMinutos() + " minutos");
 
     }
 }
